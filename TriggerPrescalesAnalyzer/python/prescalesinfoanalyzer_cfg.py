d2efe8a6b7b31c35da32bad567cfb0548a138775
rescobar7 <rescobar7@outlook.es>
2019-07-16 12:16:45 +0200

    JSON files, first try with wildcards

--- TriggerPrescalesAnalyzer/python/prescalesinfoanalyzer_cfg.py
+++ TriggerPrescalesAnalyzer/python/prescalesinfoanalyzer_cfg.py
@@ -1,4 +1,8 @@
 import FWCore.ParameterSet.Config as cms
+import FWCore.PythonUtilities.LumiList as LumiList 
+
+goodJSON = 'Cert_160404-180252_7TeV_ReRecoNov08_Collisions11_JSON.txt'
+myLumis = LumiList.LumiList(filename = goodJSON).getCMSSWString().split(',')
 
 process = cms.Process("TriggerInfo")
 
@@ -8,10 +12,42 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-' root://eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/10000/06E59C16-0A40-E311-B387-02163E008D9F.root' 
+#' root://eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/10000/06E59C16-0A40-E311-B387-02163E008D9F.root', #Not good quality
+#'root://eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/000D4260-D23E-E311-A850-02163E008D77.root',
+'root://eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/00E41BFD-D93E-E311-A91D-0025901D5DEC.root',
+#'root:////eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/022B6AEB-173F-E311-B17D-0025904A90CA.root',
+#'root:////eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/02484AF2-E63E-E311-A3F6-003048D2BEE6.root',
+#'root:////eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/024E0F96-B93E-E311-8A3C-02163E008DA1.root',
+#'root:////eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/027308C4-C73E-E311-8A0C-003048F23FA8.root',
+#'root:////eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/02751E29-D73E-E311-B249-02163E00CD78.root',
+#'root:////eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/0278353F-F23E-E311-AEED-02163E00C482.root',
+#'root:////eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/02C290BF-E03E-E311-913B-C860001BD86A.root',
+#'root:////eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/02D19806-DD3E-E311-BC8A-002481E0D1F2.root',
+#'root:////eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/02E16C94-0B3F-E311-9F2F-02163E00C4FA.root',
+#'root:////eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/02E91997-EF3E-E311-8520-0025901AF660.root',
+#'root:////eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/0420D5E4-B23E-E311-9F88-02163E00BF97.root',
+#'root:////eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/0422E58A-A53E-E311-9A3E-02163E00ADFE.root',
+#'root:////eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/04446C4B-183F-E311-AB6F-02163E008D93.root',
+#'root:////eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/0444C885-EB3E-E311-AD67-C860001BD834.root',
+#'root:////eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/0444F8BF-DB3E-E311-9729-003048F02C66.root',
+#'root:////eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/0471E862-D03E-E311-B57D-02163E00A38A.root',
+#'root:////eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/04A4EED5-053F-E311-A2AF-003048F1B904.root',
+#'root:////eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/04CD866D-EE3E-E311-B148-003048F23676.root',
+#'root:////eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/04D9EECE-F03E-E311-910B-02163E0079DA.root',
+#'root://eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/04DE8625-BC3E-E311-8E8B-003048F1C9AC.root',
+#'root://eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/04FC376C-053F-E311-9BAC-02163E0079D5.root',
+#'root://eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/06318261-F83E-E311-B60D-003048CF92B8.root',
+#'root://eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/06E23025-BC3E-E311-BCD0-003048F01100.root',
+#'root://eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/06EA1332-D83E-E311-84BD-02163E00BA2B.root',
+#'root://eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/0811F6D0-F63E-E311-8119-003048F0E3E2.root',
+#'root://eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/0821EF2C-F13E-E311-82A6-0025901AF58A.root',
+#'root://eospublic.cern.ch//eos/opendata/cms/Run2011A/Jet/AOD/12Oct2013-v1/20000/083338D7-C83E-E311-A6BA-BCAEC518FF52.root'
 #    'file:00082EAF-C03D-E311-8E53-003048F00B1C.root' 
     )
 )
+
+process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange()
+process.source.lumisToProcess.extend(myLumis)
 
 #needed to get the actual prescale values used from the global tag
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
@@ -20,20 +56,29 @@ process.GlobalTag.globaltag = 'FT_53_LV5_AN1::All'
 
 #configure the analyzer
 process.gettriggerinfo = cms.EDAnalyzer('TriggerPrescalesAnalyzer'
-                              #,JetInputCollection = cms.InputTag("ak5PFJets")
-                              ,JetInputCollection = cms.InputTag("ak5CaloJets")
-                              ,processName = cms.string("HLT")
-                              ,triggerName = cms.string("HLT_Jet110_v3")         
-                              ,triggerResults = cms.InputTag("TriggerResults","","HLT")
-                              ,triggerEvent = cms.InputTag("hltTriggerSummaryAOD","","HLT")
-                              ,filterName = cms.string("hltSingleJet190Regional")
-                              )
-
+	#To have fun with wildcards
+	#triggerSelection = cms.vstring('(HLT_Jet190_v*)'), #string 
+	#triggerConfiguration =  cms.PSet(
+		#hltResults = cms.InputTag('TriggerResults','','HLT'),
+		#l1tResults = cms.InputTag('gtDigis'),
+		#daqPartitions = cms.uint32(1),
+		#l1tIgnoreMask = cms.bool( False ),
+		#l1techIgnorePrescales = cms.bool( False ),
+		#throw  = cms.bool( True )
+	#)
+    #,JetInputCollection = cms.InputTag("ak5PFJets")
+    ,JetInputCollection = cms.InputTag("ak5CaloJets")
+    ,processName = cms.string("HLT")
+    #,triggerName = cms.string("HLT_Jet190_v1")
+    ,triggerResults = cms.InputTag("TriggerResults","","HLT")
+    ,triggerEvent = cms.InputTag("hltTriggerSummaryAOD","","HLT")
+    ,filterName = cms.string("hltSingleJet190Regional")
+)
+                              
 #configure the TFileservice, in order to save histograms.
 process.TFileService = cms.Service("TFileService",
               fileName = cms.string('histo.root')
                                    )
                                    
-                                                              
 process.triggerinfo = cms.Path(process.gettriggerinfo)
 process.schedule = cms.Schedule(process.triggerinfo)
--- TriggerPrescalesAnalyzer/src/TriggerPrescalesAnalyzer.cc
+++ TriggerPrescalesAnalyzer/src/TriggerPrescalesAnalyzer.cc
@@ -36,6 +36,11 @@
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 
+////classes to have fun with wildcards
+//#include "HLTrigger/HLTcore/interface/TriggerExpressionData.h"
+//#include "HLTrigger/HLTcore/interface/TriggerExpressionEvaluator.h"
+//#include "HLTrigger/HLTcore/interface/TriggerExpressionParser.h"
+
 #include<vector>
 
 //**
@@ -91,8 +96,8 @@ class TriggerPrescalesAnalyzer : public edm::EDAnalyzer {
 	//void analyzeJets(const edm::Event& iEvent, const edm::Handle<reco::PFJetCollection> &jets);
 	void analyzeJets(const edm::Event& iEvent, const edm::Handle<reco::CaloJetCollection> &jets);//Jet analysis
 	bool checkTriggerPass(const edm::Event& iEvent, const std::string& triggerName);//Check if the trigger passed
-	void analyzeTriggObject(const edm::Event& iEvent, const edm::Handle<trigger::TriggerEvent> &trigEvent, const edm::InputTag &trigEventTag_);//declare a function to do the trigger analysis
-	       
+	void analyzeTriggObject(const edm::Event& iEvent, const edm::Handle<trigger::TriggerEvent> &trigEvent);//declare a function to do the trigger analysis
+	
 	//declare the variables for save data in the ROOT files
 	TFile *myfile;
 	TTree *mytree;
@@ -109,6 +114,10 @@ class TriggerPrescalesAnalyzer : public edm::EDAnalyzer {
 	std::vector<float> trigobj_pt;
 	
 	edm::Service<TFileService> fs;//to access the TFileService object in a framework module
+	
+	////To have fun with wild cards
+	//triggerExpression::Data m_triggerCache;
+	//std::unique_ptr<triggerExpression::Evaluator> m_triggerSelector;
 	
 	// from HLTEventAnalyzerAOD.h
 	/// module config parameters - Have to be in order
@@ -128,9 +137,12 @@ class TriggerPrescalesAnalyzer : public edm::EDAnalyzer {
 
 //This should match your configuration python file
 TriggerPrescalesAnalyzer::TriggerPrescalesAnalyzer(const edm::ParameterSet& iConfig):
+//To have fun with wild cards
+//m_triggerCache(iConfig.getParameterSet("triggerConfiguration") ),
+//m_triggerSelector(triggerExpression::parse( iConfig.getParameter<std::string>("triggerSelection") ) ),
 jetInput_(iConfig.getParameter<edm::InputTag>("JetInputCollection")),
 processName_(iConfig.getParameter<std::string>("processName")),
-triggerName_(iConfig.getParameter<std::string>("triggerName")),
+triggerName_("HLT_Jet190_v1"),
 triggerResultsTag_(iConfig.getParameter<edm::InputTag>("triggerResults")),
 triggerEventTag_(iConfig.getParameter<edm::InputTag>("triggerEvent")),
 filterName_(iConfig.getParameter<std::string>("filterName"))
@@ -148,13 +160,10 @@ filterName_(iConfig.getParameter<std::string>("filterName"))
 		<<"		Jet Input Collection = "<< jetInput_.encode() << endl
 		<<"		ProcessName = "<< processName_ << endl
 		<<"		TriggerName = "<< triggerName_ << endl
+		//<<"		TriggerName = "<< m_triggerSelector << endl//To have fun with wild cards
 		<<"		TriggerResultsTag = "<< triggerResultsTag_.encode() << endl
 		<<"		TriggerEventTag = "<< triggerEventTag_.encode() << endl
 		<<"		FilterName = "<<filterName_<<endl;
-		
-	
-		
-	
 }
 
 TriggerPrescalesAnalyzer::~TriggerPrescalesAnalyzer()
@@ -172,10 +181,10 @@ void TriggerPrescalesAnalyzer::beginJob()//Everything ROOT related
 	myfile = new TFile("TriggerPrescaleExample.root","RECREATE");
 	mytree = new TTree("mytree","Trigger accept information");
 
-	trig_vs_pt = fs->make <TH1D>("Trig vs pt", "Trig vs pt ", 100,0,500);
+	trig_vs_pt = fs->make <TH1D>("Trig vs pt", "Trig vs pt ", 100,0,2000);
 	mytree->Branch("jet_pt",&jet_pt);
 	
-	trighist_pt = fs->make <TH1D>("trighist_pt", "obj pt ", 100,0,5000 );
+	trighist_pt = fs->make <TH1D>("trighist_pt", "obj pt ", 100,0,2000);
 	mytree->Branch("trigobj_pt",&trigobj_pt);
 }//-----------------------------------------------------------beginJob()
 
@@ -194,14 +203,23 @@ void TriggerPrescalesAnalyzer::beginRun(edm::Run const& iRun, edm::EventSetup co
 		if (changed) {
 			// check if trigger name in (new) config
 			if (triggerName_!="@") {
+			//if (m_triggerSelector!="@") {//To have fun with wild cards
 				const unsigned int n(hltConfig_.size());
 				const unsigned int triggerIndex(hltConfig_.triggerIndex(triggerName_));
+				//const unsigned int triggerIndex(hltConfig_.triggerIndex(m_triggerSelector));//To have fun with wild cards
 				if (triggerIndex>=n) {
 					cout<<"HLTEventAnalyzerAOD::analyze:"
 						<<" TriggerName "<<triggerName_ 
+						//<<" TriggerName "<<m_triggerSelector//To have fun with wild cards
 						<<" not available in (new) config!"<<endl;
 					cout<<"Available TriggerNames are: "<<endl;
 					hltConfig_.dump("Triggers");
+					if (triggerName_ == "HLT_Jet190_v2") {
+						triggerName_ = "HLT_Jet190_v3";
+					}
+					if (triggerName_ == "HLT_Jet190_v1") {
+						triggerName_ = "HLT_Jet190_v2";
+					}
 				}	
 			}	
 		}	
@@ -235,9 +253,17 @@ void TriggerPrescalesAnalyzer::analyze(const edm::Event& iEvent, const edm::Even
 	
 	assert(triggerResultsHandle_->size()==hltConfig_.size());
 	
+	//if (m_triggerSelector and m_triggerCache.setEvent(event, setup)) {//To have fun with wild cards
+		//// if the L1 or HLT configurations have changed, (re)initialize the filters (including during the first event)
+		//if (m_triggerCache.configurationUpdated())
+		//m_triggerSelector ->init(m_triggerCache);
+		//bool result = (*m_triggerSelector)(m_triggerCache);
+	//}
+	
 	if (!checkTriggerPass(iEvent, triggerName_)){
+	//if (!checkTriggerPass(iEvent, m_triggerSelector)){//To have fun with wild cards
 		return;
-	}   	 
+	}
 	
 	cout<<"********************************************************************************************************************Hey I passed the trigger"<<endl;
 	
@@ -250,12 +276,33 @@ void TriggerPrescalesAnalyzer::analyze(const edm::Event& iEvent, const edm::Even
 	
 	Handle<trigger::TriggerEvent> mytrigEvent;//Declare the handle (container) to store trigger objects.
 	iEvent.getByLabel(triggerEventTag_,mytrigEvent);
-	analyzeTriggObject(iEvent,mytrigEvent,triggerEventTag_);   	
+	analyzeTriggObject(iEvent,mytrigEvent);
 	
 	mytree->Fill(); //Now, the information is stored.
 	
 	return;
 }//------------------------------------------------------------analyze()
+
+bool TriggerPrescalesAnalyzer::checkTriggerPass(const edm::Event& iEvent, const std::string& triggerName)
+{
+	using namespace std;
+	
+	const unsigned int triggerIndex(hltConfig_.triggerIndex(triggerName));//Get the trigger index for the current trigger
+	
+	assert(triggerIndex==iEvent.triggerNames(*triggerResultsHandle_).triggerIndex(triggerName));//check that the trigger in the event and in the configuration agree
+	
+	bool acceptedTrigger = triggerResultsHandle_->accept(triggerIndex);//Was the trigger accepted? 1 == True, 0 == False.
+	bool runTrigger = triggerResultsHandle_->wasrun(triggerIndex);//Was the trigger run? 1 == True, 0 == False.
+	bool errorTrigger = triggerResultsHandle_->error(triggerIndex);//Was the trigger with an error? 1 == True, 0 == False.
+	
+	cout<<"Currently analyzing trigger: "<<triggerName<<endl;//Prints trigger name
+	
+	cout<<"The trigger was accepted: "<<acceptedTrigger<<endl
+		<<" The trigger was run: "<<runTrigger<<endl
+		<<" The trigger had an error: "<<errorTrigger<<endl;
+	
+	return acceptedTrigger;	
+}//---------------------------------------------------checkTriggerPass()
 
 void TriggerPrescalesAnalyzer::analyzeJets(const edm::Event& iEvent, const edm::Handle<reco::CaloJetCollection> &jets)
 {
@@ -266,7 +313,6 @@ void TriggerPrescalesAnalyzer::analyzeJets(const edm::Event& iEvent, const edm::
 	
 	if(jets.isValid()) {
 		numjet=(*jets).size();// get the number of jets in the event
-		//int myidx = 0;
 		
 		for (reco::CaloJetCollection::const_iterator itjet=jets->begin(); itjet!=jets->end(); ++itjet) {
 			jet_pt.push_back(itjet->pt());//Fill the vector
@@ -275,15 +321,16 @@ void TriggerPrescalesAnalyzer::analyzeJets(const edm::Event& iEvent, const edm::
 				<<" -- Px: "<<itjet->px()
 				<<" -- Py: "<<itjet->py()
 				<<" -- Pz: "<<itjet->pz()
-				<<" -- Energy: "<<itjet->energy()<<endl;
-			
-			//if (myidx==0) {trig_vs_pt->Fill(itjet->pt());}
-			//myidx++;    	    
+				<<" -- Energy: "<<itjet->energy()<<endl;			
 		}
 		
 		//std::cout<<"Maximum value: "<<*std::max_element(jet_pt.begin(), jet_pt.end())<<"First value: "<<jet_pt.front()<<std::endl; //Shows the max and first value of the vector
 		
-		trig_vs_pt->Fill(*std::max_element(jet_pt.begin(), jet_pt.end()));
+		trig_vs_pt->Fill(*std::max_element(jet_pt.begin(), jet_pt.end()));//Fill(*std::max_element(jet_pt.begin(), jet_pt.end()), weight)
+		
+		if (*std::max_element(jet_pt.begin(), jet_pt.end()) < 100) {
+		cout<<"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$Look at me"<<endl;
+		}
 	}
 }//--------------------------------------------------------analyzeJets()
 
@@ -318,33 +365,13 @@ void TriggerPrescalesAnalyzer::analyzeJets(const edm::Event& iEvent, const edm::
 	//}
 //}//--------------------------------------------------------analyzeJets()
 
-bool TriggerPrescalesAnalyzer::checkTriggerPass(const edm::Event& iEvent, const std::string& triggerName)
+void TriggerPrescalesAnalyzer::analyzeTriggObject(const edm::Event& iEvent, const edm::Handle<trigger::TriggerEvent> &trigEvent)
 {
 	using namespace std;
-	
-	const unsigned int triggerIndex(hltConfig_.triggerIndex(triggerName));//Get the trigger index for the current trigger
-	
-	assert(triggerIndex==iEvent.triggerNames(*triggerResultsHandle_).triggerIndex(triggerName));//check that the trigger in the event and in the configuration agree
-	
-	bool acceptedTrigger = triggerResultsHandle_->accept(triggerIndex);//Was the trigger accepted? 1 == True, 0 == False.
-	bool runTrigger = triggerResultsHandle_->wasrun(triggerIndex);//Was the trigger run? 1 == True, 0 == False.
-	bool errorTrigger = triggerResultsHandle_->error(triggerIndex);//Was the trigger with an error? 1 == True, 0 == False.
-	
-	//cout<<"Currently analyzing trigger: "<<triggerName<<endl;//Prints trigger name
-	
-	cout<<"The trigger was accepted: "<<acceptedTrigger<<endl
-		<<" The trigger was run: "<<runTrigger<<endl
-		<<" The trigger had an error: "<<errorTrigger<<endl;
-	
-	return acceptedTrigger;	
-}//---------------------------------------------------checkTriggerPass()
-
-void TriggerPrescalesAnalyzer::analyzeTriggObject(const edm::Event& iEvent, const edm::Handle<trigger::TriggerEvent> &trigEvent, const edm::InputTag &trigEventTag_)
-{
 	numtrigobj = 0;
 	trigobj_pt.clear();
 
-	trigger::size_type filterIndex = trigEvent->filterIndex(edm::InputTag(filterName_,"",trigEventTag_.process()));
+	trigger::size_type filterIndex = trigEvent->filterIndex(edm::InputTag(filterName_,"",triggerEventTag_.process()));
 	
 	if(filterIndex < trigEvent->sizeFilters()) {
 		const trigger::Keys& trigKeys = trigEvent->filterKeys(filterIndex); 
@@ -356,8 +383,9 @@ void TriggerPrescalesAnalyzer::analyzeTriggObject(const edm::Event& iEvent, cons
 			trigobj_pt.push_back(trigobj.pt());
 
 			trighist_pt->Fill(trigobj.pt());
+			cout<<"Trigger Pt: "<<trigobj.pt()<<endl;
 			numtrigobj=numtrigobj+1;
-		}	
+		}
 	}//end filter size check
 }//-------------------------------------------------analyzeTriggObject()
 
