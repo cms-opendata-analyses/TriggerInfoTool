d20cf39b2f03ed0c68a93e6f0deaa72a44c02029
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2020-08-17 15:38:26 +0200

    Update moduleinfoanalyzer_cfg.py

--- ModuleInTriggerAnalyzer/python/moduleinfoanalyzer_cfg.py
+++ ModuleInTriggerAnalyzer/python/moduleinfoanalyzer_cfg.py
@@ -3,7 +3,6 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
