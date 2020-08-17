46b0e5507819a42dc007b932870db20d5e9701ec
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2020-08-17 17:28:54 +0200

    Update triggmatchinganalyzer_cfg.py

--- TriggerMatchingAnalyzer/python/triggmatchinganalyzer_cfg.py
+++ TriggerMatchingAnalyzer/python/triggmatchinganalyzer_cfg.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 
